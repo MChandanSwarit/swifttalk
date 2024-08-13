ee63cc1cf05ba0f0a664339167f2064378518f76
MChandanSwarit <144525956+MChandanSwarit@users.noreply.github.com>
2024-08-13 20:55:47 +0530

    Update MessageContainer.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -21,7 +21,7 @@ const MessageContainer = () => {
 				<>
 					{/* Header */}
 					<div className='bg-slate-500 px-4 py-2 mb-2'>
-						<span className='label-text'>To:</span>{" "}
+						<span className='label-text'>To: </span>{" "}
 						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
 					</div>
 					<Messages />
