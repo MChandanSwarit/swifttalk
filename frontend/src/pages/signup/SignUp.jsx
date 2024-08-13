f39f1977cfef9658db0ac1a856f25a48d4bdb10c
MChandanSwarit <144525956+MChandanSwarit@users.noreply.github.com>
2024-08-13 20:54:20 +0530

    Update SignUp.jsx

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -27,7 +27,7 @@ const SignUp = () => {
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Sign Up <span className='text-blue-500'> ChatApp</span>
+					Sign Up <span className='text-blue-500'> SwiftTalk</span>
 				</h1>
 
 				<form onSubmit={handleSubmit}>
