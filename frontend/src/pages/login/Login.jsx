84bdfec15d461138df672f7b5f17f858db253699
MChandanSwarit <144525956+MChandanSwarit@users.noreply.github.com>
2024-08-13 20:54:48 +0530

    Update Login.jsx

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
 					Login
-					<span className='text-blue-500'> ChatApp</span>
+					<span className='text-blue-500'> SwiftTalk</span>
 				</h1>
 
 				<form onSubmit={handleSubmit}>
