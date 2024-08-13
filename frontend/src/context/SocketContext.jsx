7a7e1f5e9d66824cb07d74287f46b64d71fb5c85
MChandanSwarit <144525956+MChandanSwarit@users.noreply.github.com>
2024-08-13 20:45:03 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://swifttalk-487i.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
